013d43aa165948a4245f0562101e958130a7f12b
Grant Hutchins <pair+grant@pivotallabs.com>
2010-11-18 12:28:41 -0500

    Check if the table exists before dropping it.

--- lib/with_model.rb
+++ lib/with_model.rb
@@ -8,7 +8,7 @@ module WithModel
   def with_table(name, &block)
     connection = ActiveRecord::Base.connection
     before do
-      connection.drop_table(name) rescue nil
+      connection.drop_table(name) if connection.table_exists?(name)
       connection.create_table(name, &block)
     end
 
