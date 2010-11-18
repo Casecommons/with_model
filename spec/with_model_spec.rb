b7f22bf147673589d6725668c273c511da78e4e0
Grant Hutchins & Peter Jaros <pair+grant+pjaros@pivotallabs.com>
2010-11-18 11:49:24 -0500

    Add more test coverage.

--- spec/with_model_spec.rb
+++ spec/with_model_spec.rb
@@ -1,21 +1,29 @@
+# In your spec_helper.rb:
+
 require "active_record"
 require "with_model"
 
 if defined?(RSpec)
+  # For RSpec 2 users.
   RSpec.configure do |config|
     config.extend WithModel
   end
 else
+  # For RSpec 1 users.
   Spec::Runner.configure do |config|
     config.extend WithModel
   end
 end
 
+# WithModel requires ActiveRecord::Base.connection to be established.
+# If ActiveRecord already has a connection, as in a Rails app, this is unnecessary.
 ActiveRecord::Base.establish_connection(:adapter  => 'sqlite3', :database => ":memory:")
-connection = ActiveRecord::Base.connection
-connection.execute("SELECT 1")
 
-describe "an ActiveRecord model" do
+
+
+# In your spec:
+
+describe "a temporary ActiveRecord model created with with_model" do
   with_model :blog_post do
     table do |t|
       t.string 'title'
@@ -30,9 +38,24 @@ describe "an ActiveRecord model" do
     end
   end
 
-  it "should save properly" do
+  it "should act like a normal ActiveRecord model" do
+    record = @blog_post.create!(:title => 'New blog post', :content => "Hello, world!")
+
+    record.reload
+
+    record.title.should == 'New blog post'
+    record.content.should == 'Hello, world!'
+    record.updated_at.should be_present
+
+    record.destroy
+
     lambda {
-      @blog_post.create!(:title => 'New blog post', :content => "Hello, world!")
-    }.should_not raise_error
+      record.reload
+    }.should raise_error(ActiveRecord::RecordNotFound)
+  end
+
+  it "should have methods defined in its model block" do
+    puts @blog_post.new(:title => 'New blog post').class.instance_methods(false)
+    @blog_post.new(:title => 'New blog post').fancy_title.should == "Title: New blog post"
   end
 end
