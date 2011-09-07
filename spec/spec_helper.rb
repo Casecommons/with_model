6a691962db7ce86356dd6f5568076f1a40481e89
Grant Hutchins <nertzy@gmail.com>
2011-09-06 21:27:53 -0400

    Use jdbcsqlite3 for specs in JRuby

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,9 +13,12 @@ else
   end
 end
 
+jruby = RUBY_PLATFORM =~ /\bjava\b/
+adapter = jruby ? "jdbcsqlite3" : "sqlite3"
+
 # WithModel requires ActiveRecord::Base.connection to be established.
 # If ActiveRecord already has a connection, as in a Rails app, this is unnecessary.
-ActiveRecord::Base.establish_connection(:adapter => 'sqlite3', :database => ":memory:")
+ActiveRecord::Base.establish_connection(:adapter => adapter, :database => ":memory:")
 
 # For readme_spec.rb
 module SomeModule; end
