3b37ae388f9fe2dd7ff7b54bb99b4a1f9d42b950
Andrew Marshall <andrew@johnandrewmarshall.com>
2014-05-19 22:26:05 -0400

    Fix uninitialized instance variable warning

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -44,7 +44,7 @@ module SpecHelper
     if ::ActiveRecord::VERSION::STRING >= '4.1.0'
       require 'minitest'
       include Minitest::Assertions
-      def assertions; @assertions || 0; end
+      def assertions; @assertions ||= 0; end
       def assertions= value; @assertions = value; end
     else
       require 'test/unit/assertions'
