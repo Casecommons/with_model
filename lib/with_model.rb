94a106b27ca0e76ed4f7d4f3102ac152aaad1b62
Andrew Marshall <andrew@johnandrewmarshall.com>
2014-06-06 13:54:08 -0400

    Allow before/after to naturally use their own defaults
    Instead of explicitly specifying that it should be :each.
    
    (#18)

--- lib/with_model.rb
+++ lib/with_model.rb
@@ -6,32 +6,32 @@ require 'with_model/version'
 module WithModel
   def with_model(name, options = {}, &block)
     options = options.dup
-    scope = options.delete(:scope) { :each }
+    scope = options.delete(:scope)
 
     model = Model.new name, options
     dsl = Model::DSL.new model
     dsl.instance_exec(&block) if block
 
-    before scope do
+    before(*scope) do
       model.create
     end
 
-    after scope do
+    after(*scope) do
       model.destroy
     end
   end
 
   def with_table(name, options = {}, &block)
     options = options.dup
-    scope = options.delete(:scope) { :each }
+    scope = options.delete(:scope)
 
     table = Table.new name, options, &block
 
-    before scope do
+    before(*scope) do
       table.create
     end
 
-    after scope do
+    after(*scope) do
       table.destroy
     end
   end
